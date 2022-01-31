86cf6a2ac3f500262ee69f2728b16a7ab3597a13
vikyjaware <vikyjaware@gmail.com>
2022-01-31 23:27:52 +0530

    [Viky] Add : Folder structure added

---
+++ Logicalprograms/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprograms
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter the number of terms required ");
+            int n = Convert.ToInt32(Console.ReadLine()) ;
+            int i, firstnum = 1, secondnum = 1, thirdnum;
+            Console.Write(firstnum + "\t" +secondnum );
+            for( i=2; i<n; i++)
+            {
+                thirdnum = firstnum + secondnum;
+                firstnum = secondnum;
+                secondnum= thirdnum;
+                Console.Write("\t" +thirdnum );
+            }
+        }
+    }
+}
