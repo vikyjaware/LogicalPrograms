db039b73ae4d9b061be44b484bfcb602f3c3278b
vikyjaware <vikyjaware@gmail.com>
2022-02-03 00:02:42 +0530

    [Viky] Add : Folder structure added

--- Logicalprograms/Program.cs
+++ Logicalprograms/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Logicalprograms;
 
-PrimeNumber primnum=new PrimeNumber();
-primnum.Prime();
+ReverseNumber reversenum=new ReverseNumber();
+reversenum.reverse();
---
+++ Logicalprograms/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprograms
+{
+    public class ReverseNumber
+    {
+        public void reverse()
+        {
+            Console.WriteLine("Enter the number you want to reverse...");
+            int n = Convert.ToInt32(Console.ReadLine()) ;
+            int result = 0;
+            while(n > 0)
+            {
+                int reminder = n % 10;
+                result = result * 10 + reminder;
+                n /= 10;
+            }
+            Console.WriteLine("The Reverse of Number "+ n + " is " + result);
+        }
+    }
+}
